a88a3c52a71fb8702130d4c57acd49efbe277790
zaragozabraham <abrahamezg@gmail.com>
2022-04-21 03:27:38 +0000

    custom user

---
+++ backend/api/src/musicapi/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MusicStore API",
+      default_version='v1',
+      description="Music Store API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@musicstore.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+   #  path('admin/', admin.site.urls),
+    path('api_auth/', include('rest_framework.urls')),
+    path('users/', include('musicapi.users.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
---
+++ backend/api/src/musicapi/users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+	email = models.CharField(max_length=255, unique=True)
+	username = models.CharField(max_length=255, unique=True)
+
+	class Type(models.IntegerChoices):
+		CUSTOMER = 1
+		ADMIN = 2
+
+	type = models.IntegerField(choices = Type.choices, default = 1)
