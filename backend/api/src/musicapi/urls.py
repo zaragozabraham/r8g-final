06efbd31efe4b0701d0438ef8ce26762b3e9b079
zaragozabraham <abrahamezg@gmail.com>
2022-04-21 03:39:46 +0000

    admin view added

--- backend/api/src/musicapi/urls.py
+++ backend/api/src/musicapi/urls.py
@@ -18,7 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-   #  path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api_auth/', include('rest_framework.urls')),
     path('users/', include('musicapi.users.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
