f49799544fd70660695989bbe974df80d082acac
zaragozabraham <abrahamezg@gmail.com>
2022-05-11 05:06:19 +0000

    push

--- backend/api/src/musicapi/songs/serializers.py
+++ backend/api/src/musicapi/songs/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import *
+from musicapi.artists.serializers import ArtistSerializer
+from musicapi.albums.serializers import AlbumSerializer
 
 class SongSerializer(serializers.ModelSerializer):
-
+    # artists = ArtistSerializer(many=True, read_only=True)
+    # album = AlbumSerializer(many=True, read_only=True)
     class Meta:
         model = Song
         fields = ['id', 'name', 'duration', 'releaseDate', 'audioFile', 'digitalPrice', 'artists', 'album', 'ownedSongID', 'playlistSongID', 'orderSongID']
